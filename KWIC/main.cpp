627136ccb71716338a82579b367a8af0bb3eef61
Fadil Smajilbasic <smajilbaicfadil@gmail.com>
2021-11-22 23:30:56 +0100

    project update

--- KWIC/main.cpp
+++ KWIC/main.cpp
@@ -1,31 +1,12 @@
 #include "word.h"
-
+#include "kwic.h"
 #include <iostream>
 #include <sstream>
 
 int main(){
-	std::istringstream input{"compl33tely ~ weird !!?!! 4matted in_put"};
-	text::Word w{};
 
-	input >> w;
-	std::cout << w << "\n";
 
-	input >> w;
-	std::cout << w << "\n";
 
-	input >> w;
-	std::cout << w << "\n";
-
-	input >> w;
-	std::cout << w << "\n";
-
-	input >> w;
-	std::cout << w << "\n";
-
-	input >> w;
-	std::cout << w << "\n";
-
-	input >> w;
 
 	text::Word v{"Aa"};
 	text::Word x{"aab"};
