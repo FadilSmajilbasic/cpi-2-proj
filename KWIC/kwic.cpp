d6f9f53d3b1c70c1af60ce86cec5de593ddd2ab7
Fadil Smajilbasic <smajilbaicfadil@gmail.com>
2021-11-22 23:30:46 +0100

    Added draft kwic algorithm

--- KWIC/kwic.cpp
+++ KWIC/kwic.cpp
@@ -1,5 +1,43 @@
 #include "kwic.h"
+#include "word.h"
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <string>
 
 namespace text {
+
+
+void kwic(std::istream &is) {
+	text::Word words { };
+
+	is >> words;
+
+	std::vector<std::string> wordsVector { };
+	std::ostringstream out { };
+
+	out << words;
+
+	wordsVector.push_back(out.str());
+
+	std::vector<std::vector<std::string>> kwicFinished = getTitles(wordsVector);
+
+
+}
+
+std::vector<std::vector<std::string>> getTitles(std::vector<std::string> words) {
+
+	int modVal = words.size() - 1;
+
+	std::vector<std::vector<std::string>> result;
+
+	for (int i = 0; i < modVal; i++) {
+		for (int k = 0; k < modVal; k++) {
+			result[i][k] = words[(i+k)%modVal];
+		}
+	}
+	return result;
+}
 
 }
--- KWIC/kwic.h
+++ KWIC/kwic.h
@@ -1,8 +1,17 @@
 #ifndef KWIC_H_
 #define KWIC_H_
 
-namespace text{
+#include <iostream>
+#include <vector>
 
+namespace text{
+class Kwic{
+public:
+	void kwic(const std::istream in);
+
+	std::vector<std::vector<std::string>> getTitles(std::vector<std::string> words) ;
+
+};
 }
 
 
